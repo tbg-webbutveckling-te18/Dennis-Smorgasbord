090728f57d5a73ba0f3ebc17d31c84ea0a900bd5
Dennis Lundberg <Dennis.Lundberg87@gmail.com>
2020-09-25 15:10:02 +0200

    finished if-else and started for

--- projects/Webbutveckling-2/2-JavaScript/assets/js/1-ifelse.js
+++ projects/Webbutveckling-2/2-JavaScript/assets/js/1-ifelse.js
@@ -22,3 +22,97 @@ if (isSunny == true) {
 } else {
 	console.log('Put on some warm clothes today.');
 }
+
+// Step 1
+var d = new Date();
+if (d.getHours() < 18) {
+	console.log('Good day!');
+} else {
+	console.log('Good evening!');
+}
+
+// Step 2
+if (d.getHours() < 10) {
+	console.log('Good morning!');
+} else if (d.getHours() < 18) {
+	console.log('Good day!');
+} else {
+	console.log('Good evening!');
+}
+
+// Step 3
+if (d.getHours() < 10) {
+	if (d.getMonth() > 9 || d.getMonth() < 2) {
+		console.log('Go ahead and sleep like a bear!');
+	} else {
+		console.log('Good morning!');
+	}
+} else if (d.getHours() < 18) {
+	console.log('Good day!');
+} else {
+	console.log('Good evening!');
+}
+
+/*
+You can use multiple if...else…if statements, as in the previous chapter, 
+to perform a multiway branch. However, this is not always the best solution, 
+especially when all of the branches depend on the value of a single variable.
+*/
+
+var grade = 'A';
+switch (grade) {
+	case 'A':
+		console.log('Great job!');
+		break;
+	case 'B':
+		console.log('Good job!');
+		break;
+	case 'C':
+		console.log('Allright!');
+		break;
+	case 'D':
+		console.log('Good!');
+		break;
+	case 'E':
+		console.log('OK!!');
+		break;
+	case 'F':
+		console.log('You did not pass!');
+		break;
+
+	default:
+		console.log('Unknown grade!');
+}
+
+// Switch statement with week days
+switch (d.getDay()) {
+	case 0:
+		console.log('Sunday');
+		break;
+
+	case 1:
+		console.log('Monday');
+		break;
+
+	case 2:
+		console.log('Tuesday');
+		break;
+
+	case 3:
+		console.log('Wednesday');
+		break;
+
+	case 4:
+		console.log('Thursday');
+		break;
+	case 5:
+		console.log('Friday');
+		break;
+
+	case 6:
+		console.log('Saturday');
+		break;
+
+	default:
+		console.log('What day?');
+}
---
+++ projects/Webbutveckling-2/2-JavaScript/assets/js/2-for.js
@@ -0,0 +1,15 @@
+/* 
+The 'for' loop is the most compact form of looping. It includes the following three important parts −
+The loop initialization where we initialize our counter to a starting value. 
+The initialization statement is executed before the loop begins.
+The test statement which will test if a given condition is true or not. 
+If the condition is true, then the code given inside the loop will be executed, otherwise the control will come out of the loop.
+The iteration statement where you can increase or decrease your counter.
+*/
+for (var i = 0; i <= 10; i++) {
+	document.writeln('Count is: ' + i + '<br>');
+}
+
+for (var count = 1; count < 13; count++) {
+	document.writeln('Count is: ' + count + '<br>');
+}
